01d40be17491c82b1c55ee1a6bbdea36254f3ec3
wangpeng1994 <947034046@qq.com>
2019-06-17 11:37:26 +0800

    build: Add code coverage details

--- jest.config.js
+++ jest.config.js
@@ -4,17 +4,21 @@
  * 否则会遵循默认的缺省配置
  */
 module.exports = {
-  verbose: true,
+  'verbose': true,
+  reporters: ['default', 'jest-junit'], // 增加额外的分析报告  
+  // 生成完整的测试覆盖率报告
   collectCoverage: true,
-  reporters: ['default', 'jest-junit'], // 增加额外的分析报表
-  // testEnvironment: 'node', // Default: 'jsdom'
+  collectCoverageFrom: ['lib/**/*.{js,jsx,ts,tsx}', '!**/node_modules/**'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+
   transform: {
-    "^.+\\.(js|jsx)$": "babel-jest",
+    '^.+\\.(js|jsx)$': 'babel-jest',
     '^.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
-    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js"
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/__mocks__/file-mock.js',
+    '\\.(css|less|sass|scss)$': '<rootDir>/test/__mocks__/object-mock.js'
   },
   setupFilesAfterEnv: ['<rootDir>test/setupTests.js']
 };
